5f0d45a08c3033b9a126aa8ee016b63b41a86db7
Alexandara <alexisrenee1@gmail.com>
2024-02-05 17:25:28 -0600

    Web scraping and information cleaning complete

--- chatbot_project/main.py
+++ chatbot_project/main.py
@@ -1,6 +1,7 @@
 import web_crawler
 import chatbot
 import sys
+import os
 
 if __name__ == '__main__':
     # Take in a system argument about whether to run the web crawler or not
@@ -11,17 +12,22 @@ if __name__ == '__main__':
     except:
         print("No valid system argument passed in.")
     if web_first:
-        starter_urls = ["https://www.d20srd.org/index.htm"]
+        starter_urls = ["https://www.d20srd.org/indexes/basicsRacesDescription.htm",
+                        "https://www.d20srd.org/indexes/classes.htm",
+                        "https://www.d20srd.org/indexes/skills.htm",
+                        "https://www.d20srd.org/srd/feats.htm"]
         crawler = web_crawler.WebCrawler(starter_urls)
         # Find URLs from starter URLs provided
-        crawler.find_urls()
-        print("Found URLs:")
-        for url in crawler.urls:
-            print(url)
+        if crawler.urls == []:
+            crawler.find_urls()
         # Scrape the data from the URLs
-        crawler.scrape_all_urls()
+        files = os.listdir("files/raw_information")
+        if len(files) == 0:
+            crawler.scrape_all_urls()
         # Clean up found data
-        crawler.clean_files()
+        clean_files = os.listdir("files/clean_information")
+        if len(files) == 0:
+            crawler.clean_files()
         # Extract important terms
         crawler.extract_important_terms()
         # Build final knowledge base
--- chatbot_project/web_crawler.py
+++ chatbot_project/web_crawler.py
@@ -2,21 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import urllib.request
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+import os
 
 class WebCrawler():
 	def __init__(self, urls):
 		print("Initializing Web Crawler...")
 		self.starter_urls = urls
-		self.urls = []
+		try:
+			self.urls = self.read_file_into_list("files/urls/urls.txt")
+		except:
+			self.urls = []
 
+	# Instructions:
 	# Start with 1-3 URLs that represent a topic (a sport, a celebrity, a place, etc.) and crawl to
 	# find 15 – 25 relevant URLs. Make sure that some of the URLs are outside the original
 	# starter URLs.
 	def find_urls(self):
+		"""
+		Given a starter URL, this finds all URLs from the starter URL and also
+		25 URLs from each of those URLs.
+		"""
 		print("Finding URLs from starter URLs...")
-		first_level_urls = []
+		# These excluded URLs are mostly randomized generators
 		excluded_urls = self.read_file_into_list("files/urls/bad_urls.txt")
+		# Get every URL from the starter URL pages
 		for url in self.starter_urls:
 			r = requests.get(url)
 
@@ -28,92 +38,158 @@ class WebCrawler():
 				path = link.get('href')
 				if path and path.startswith('/'):
 					path = urljoin(url, path)
-				first_level_urls.append(path)
+				self.urls.append(self.remove_after_hash(path))
 
-		# Remove some URLs that I have selected as not very productive
-		# Mostly dice rollers and random generators
-		for url in excluded_urls:
-			if url in first_level_urls:
-				first_level_urls.remove(url)
+		# Remove excluded URLs from the final list
+		new_urls = []
+		for url in self.urls:
+			if not (url.replace("\n","") in excluded_urls):
+				new_urls.append(url)
+		self.urls = new_urls
 
-		# Find second level URLs
-		# (note, this is not the correct way to do this. Far better would be
-		# to use recursion with a set depth, but because I know the site has most things
-		# on the second level I'm just doing this twice)
-		for url in first_level_urls:
-			try:
-				r = requests.get(url)
-			except:
-				continue
-
-			data = r.text
-			soup = BeautifulSoup(data, 'html.parser')
-
-			# Find second level URLs
-			it = 0
-			for link in soup.find_all('a'):
-				path = link.get('href')
-				if path and path.startswith('/'):
-					path = urljoin(url, path)
-				self.urls.append(path)
-				it += 1
-				if it > 25:
-					break
-
-		for url in excluded_urls:
-			if url in self.urls:
-				self.urls.remove(url)
-
+		# Turn the URL list to a set and back again to remove duplicates
 		temp_set = set(self.urls)
 		self.urls = list(temp_set)
+
 		# Remove all invalid items
 		for url in self.urls:
 			try:
 				_ = requests.get(url)
 			except:
 				self.urls.remove(url)
+		# Write final URL list to file
 		self.write_list_to_file("files/urls/urls.txt", self.urls)
 
-	# Code from Dr. Mazidi's GitHub below
-	def visible(self, element):
-		if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+	# Code inspired by code from Dr. Mazidi's GitHub below
+	def is_valuable(self, element):
+		"""
+		Determines whether an element in an html file is part of useful information or not
+		:param element: element to check
+		:return: True if element is not blacklisted, False if not
+		"""
+		not_wanted = ['style', 'script', '[document]', 'head', 'title',
+		              'noscript', 'header', 'html', 'meta', 'input',
+		              '\n', 'Advertisement']
+		if element.parent.name in not_wanted:
 			return False
 		elif re.match('<!--.*-->', str(element.encode('utf-8'))):
 			return False
 		return True
 
 	def read_file_into_list(self, file):
+		"""
+		Code to read the contents of a file into a list
+		:param file: filename to read in
+		:return: a list containing the contents of each line of the
+					file as an element
+		"""
 		f = open(file, "r")
 		new_list = []
 		for line in f:
-			new_list.append(line)
+			newline = line.replace("\n", "")
+			new_list.append(newline)
 		f.close()
 		return new_list
 
 	def write_list_to_file(self, file, l):
+		"""
+		Helper function that writes a list into a file, each element of
+		the list separated by a newline
+		:param file: filename to write to
+		:param l: list to write
+		"""
 		f = open(file, "w")
 		for item in l:
-			f.write(str(item))
+			f.write(str(item.replace("\n", "")))
 			f.write("\n")
 
-	# • Write a function to loop through the URLs, scrape text off each page and write it to a
+	def remove_after_hash(self, element):
+		"""
+		This method just removes all text after a #
+		This is primarily for URLs that are duplicates but have a #section
+		indicating what part of the page to start with (useful for humans,
+		useless for a web scraper and results in duplication)
+		:param element: text to remove end of
+		:return: returns string without anything after the # (including the #)
+		"""
+		new_string = ""
+		for char in element:
+			if char != "#":
+				new_string += char
+			else:
+				break
+		return new_string
+
+	# Instruction:
+	# Write a function to loop through the URLs, scrape text off each page and write it to a
 	# file. Write each URL’s text to a separate file.
 	def scrape_all_urls(self):
+		"""
+		This method scrapes the urls in self.urls (which contains
+		URLs from files/urls/urls.txt) and puts each URL's data into
+		a separate file. This does rudimentary text cleaning primarily
+		to get only visible text information from the website.
+		"""
 		print("Scraping found URLs for data...")
+		i = 1
+		for url in self.urls:
+			# Get the information from the URL as raw HTML
+			try:
+				raw_site = requests.get(url)
+			except:
+				continue
+			html_site = raw_site.content
+			# Get a soup object to parse the html
+			soup = BeautifulSoup(html_site, 'html.parser')
+			raw_text = soup.find_all(text=True)
+			# Remove valueless items
+			final_text = []
+			for text in raw_text:
+				if self.is_valuable(text):
+					final_text.append(text)
+			# Write to file with the URL as the first  line
+			if "d20srd" in url:
+				for _ in range(111):
+					try:
+						final_text.pop(0)
+					except:
+						break
+			final_text.insert(0, url)
+			self.write_list_to_file("files/raw_information/file" + str(i) + ".txt", final_text)
+			i += 1
 
-	# • Write another function to clean up the text files. Read in each raw file and clean it up as
+	# Instruction:
+	# Write another function to clean up the text files. Read in each raw file and clean it up as
 	# much as possible with NLP techniques. If you have x files in, you will have x files out.
 	def clean_files(self):
 		print("Cleaning scraped text...")
+		files = os.listdir("files/raw_information/")
+		for file in files:
+			information = self.read_file_into_list("files/raw_information/" + file)
+			url = information.pop(0)
+			clean_text = ""
+			for info in information:
+				new_info = info.replace("\n", "")
+				if len(new_info) != 0:
+					clean_text += new_info
+					if new_info[-1] != " ":
+						clean_text += " "
+			f = open("files/clean_information/clean_" + file, "w")
+			f.write(url)
+			f.write("\n")
+			f.write(clean_text)
+			f.close()
 
-	# • Write a function to extract at least 25 important terms from the cleaned-up files using
+	# Instruction:
+	# Write a function to extract at least 25 important terms from the cleaned-up files using
 	# an importance measure such as tf-idf. You might want to lower-case everything, remove
 	# stopwords and punctuation first. Output the top 25-40 important terms.
 	# • Manually determine the top 10-15 terms based on your domain knowledge.
 	def extract_important_terms(self):
 		print("Extracting important terms...")
 
-	# • Build a searchable knowledge base of facts that a chatbot can share related to your
+	# Instruction:
+	# Build a searchable knowledge base of facts that a chatbot can share related to your
 	# important terms. The “knowledge base” can be a simple as a Python dict which you
 	# pickle or a simple sql database.
 	def build_knowledge_base(self):
